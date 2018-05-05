d7d44f7b0b8c40491389c349517b65a8b4fa6d2a
Valeri Valeriev <valericfbg@gmail.com>
2018-05-05 17:26:29 +0300

    board

--- cmpts/Board.jsx
+++ cmpts/Board.jsx
@@ -1,9 +1,46 @@
 import { Component } from 'preact';
 
+const styles = {
+	table: {
+		margin: '0 auto'
+	},
+	snake: {
+		background: 'green'
+	}
+};
+
 export default class Board extends Component {
-  render() {
-    return (
-      <div>Hello</div>
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	renderRows(r, c, x, y) {
+		const rows = [];
+
+
+		for (let rowIdx = 0; rowIdx < r; rowIdx++) {
+			const row = [];
+			
+			for (let colIdx = 0; colIdx < c; colIdx++) {
+				row.push(<td style={x === colIdx && y === rowIdx ? styles.snake : {}} />);
+			}
+
+			rows.push(<tr>{row}</tr>);
+		}
+
+		return rows;
+	}
+
+	render() {
+		const { rows, cols, snake } = this.props;
+		const { x, y } = snake.point;
+		return (
+			<table style={styles.table}>
+				<tbody>
+					{this.renderRows(rows, cols, x, y)}
+				</tbody>
+			</table>
+		);
+	}
 }
--- index.js
+++ index.js
@@ -2,20 +2,43 @@ import './style';
 import { Component } from 'preact';
 import Board from './cmpts/Board';
 
+const rows = 30; const cols = 20;
+
+class Snake {
+	constructor(point) {
+		this.point = point;
+	}
+}
+
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			score: 0
+			score: 0,
+			snake: null
 		};
 	}
+
+	componentWillMount = () => {
+		this.state.snake = new Snake(new Point(10, 10));
+	}
+
 	render() {
+		const { snake } = this.state;
+		
 		return (
 			<div>
-				<h1>Tetris</h1>
+				<h1>Snake</h1>
 				<div>Score: {this.state.score}</div>
-				<Board />
+				<Board rows={rows} cols={cols} snake={snake} />
 			</div>
 		);
 	}
